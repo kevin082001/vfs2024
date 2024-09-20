46db6a31b74876981186c2e8af591e3b83b615cf
pva-kkerzner <k.kerzner2001@gmail.com>
2024-09-20 18:57:36 +0200

    #7 Added echo command

--- src/InputParser.java
+++ src/InputParser.java
@@ -31,8 +31,10 @@ public class InputParser {
                 System.out.println("Detected 'edit' command");
                 break;
             case "echo":
-                //handle "echo"
-                System.out.println("Detected 'echo' command");
+                boolean success = echo(split);
+                if (!success) {
+                    System.out.println("Sorry, but I could not run your command.");
+                }
                 break;
             case "help":
                 //handle "help"
@@ -46,6 +48,30 @@ public class InputParser {
         //special cases: cd .. (switch to parent directory)
         //               cd root (switch to root directory)
         return 0;
+    }
+
+    private static boolean echo(String[] args) {
+        if (args.length < 2 || args.length > 3) {
+            return false;
+        }
+
+        if (args.length == 2) {
+            System.out.println(args[1]);
+            return true;
+        }
+
+        if (!Util.isNumber(args[2])) {
+            return false;
+        }
+
+        String output = args[1];
+        int times = Integer.parseInt(args[2]);
+
+        for (int i = 0; i < times; i++) {
+            System.out.println(output);
+        }
+
+        return true;
     }
 
     private static boolean isValid(String s) {
